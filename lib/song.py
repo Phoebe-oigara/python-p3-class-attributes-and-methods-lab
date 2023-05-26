bf43d9b7d3bf10a1b4a1c2e69633ba382684364c
Phoebe Oigara <phoebeoigara@gmail.com>
2023-05-26 15:44:03 +0300

    practisisng with class attributes and methods

--- lib/song.py
+++ lib/song.py
@@ -40,26 +40,22 @@ class Song:
         if artist not in cls.artists:
             cls.genres.append(artist)
 
+
     @classmethod
     def add_to_genre_count(cls):
-        cls.genre_count = {genre: cls.genres.count(genre) for genre in cls.genres}
+        for genre in cls.genres:
+            if genre in cls.genre_count:
+                cls.genre_count[genre] += 1
+            else:
+                cls.genre_count[genre] = 1
 
-
-    # @classmethod
-    # def add_to_genre_count(cls):
-    #     for genre in cls.genres:
-    #         if genre in cls.genre_count:
-    #             cls.genre_count[genre] += 1
-    #         else:
-    #             cls.genre_count[genre] = 1
-
-    # @classmethod
-    # def add_to_artist_count(cls):
-    #     for artist in cls.artists:
-    #         if artist in cls.artist_count:
-    #             cls.artist_count[artist] += 1
-    #         else:
-    #             cls.artist_count[artist] = 1
+    @classmethod
+    def add_to_artist_count(cls):
+        for artist in cls.artists:
+            if artist in cls.artist_count:
+                cls.artist_count[artist] += 1
+            else:
+                cls.artist_count[artist] = 1
 
 
 
